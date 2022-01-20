05515279df5f9325b28599d25e33e92942beb8b9
RaspBella <bencampbell3233@gmail.com>
2022-01-20 23:04:27 +0000

    Begun by writing install.sh, chroot.sh will be written next, Then I will make a function to ask what disk to install on as for now I have DISK=sdc.

---
+++ chroot.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo Soon to be
---
+++ install.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+DISK=sdc
+
+CHECK_UEFI(){
+    if [ -e /sys/firmware/efi/efivars ]; then
+    UEFI=true
+    else
+    UEFI=false
+    fi
+}
+
+PARTITIONING(){
+    UEFI_PART(){
+        fdisk /dev/$DISK << FDISK_CMDS
+        g
+        n
+
+
+        +0.5G
+        t
+        uefi
+        n
+
+
+        +4G
+        t
+
+        swap
+        n
+
+
+
+        w
+FDISK_CMDS
+    }
+
+    LEGACY_PART(){
+        fdisk /dev/$DISK << FDISK_CMDS
+        o
+        n
+
+
+
+        +4G
+        t
+        swap
+        n
+
+
+
+
+        w
+FDISK_CMDS
+    }
+
+    if [ $UEFI=true ]; then
+    UEFI_PART
+    else
+    LEGACY_PART
+    fi
+}
+
+FORMATTING(){
+    UEFI_FORMAT(){
+        mkfs.fat -F32 /dev/"$DISK"1
+        mkswap /dev/"$DISK"2
+        mkfs.ext4 /dev/"$DISK"3
+    }
+
+    LEGACY_FORMAT(){
+        mkswap /dev/"$DISK"1
+        mkfs.ext4 /dev/"$DISK"2
+    }
+
+    if [ $UEFI=true ]; then
+    UEFI_FORMAT
+    else
+    LEGACY_FORMAT
+    fi
+}
+
+MOUNTING(){
+    UEFI_MOUNT(){
+        swapon /dev/"$DISK"2
+        mount /dev/"$DISK"3 /mnt
+    }
+
+    LEGACY_MOUNT(){
+        swapon /dev/"$DISK"1
+        mount /dev/"$DISK"2 /mnt
+    }
+
+    if [ $UEFI=true ]; then
+    UEFI_MOUNT
+    else
+    LEGACY_MOUNT
+    fi
+}
+
+PACKAGES=(base linux linux-firmware nano dhcpcd iwd)
+
+#loadkeys uk
+CHECK_UEFI
+#timedatectl set-ntp true
+PARTITIONING
+FORMATTING
+MOUNTING
+reflector -c GB
+pacstrap /mnt ${PACKAGES[*]}
+genfstab -U /mnt >> /mnt/etc/fstab
+
+#Arch chroot script
+cp chroot.sh /mnt/
+arch-chroot /mnt ./chroot.sh
