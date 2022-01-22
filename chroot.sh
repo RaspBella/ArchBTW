d67830a016e8216e9d16a66258e21fdfebd9448e
RaspBella <bencampbell3233@gmail.com>
2022-01-22 23:11:22 +0000

    Now hostname is asked

--- chroot.sh
+++ chroot.sh
@@ -38,8 +38,12 @@ echo en_GB.UTF-8 > /etc/locale.gen
 locale-gen
 echo LANG=en_GB.UTF-8 > /etc/locale.conf
 echo KEYMAP=uk > /etc/vconsole.conf
-echo test > /etc/hostname
-echo -e 127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\ttest > /etc/hosts
+
+#Get desired hostname
+read -p 'Please enter a hostname: ' hostname
+
+echo $hostname > /etc/hostname
+echo -e 127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$hostname > /etc/hosts
 mkinitcpio -P
 passwd
 
